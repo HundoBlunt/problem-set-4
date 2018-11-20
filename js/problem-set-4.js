2e61cc78f9a2678af80ec1a0ff4fb9b92a4a7dea
Tyler Nguyen <43753707+HundoBlunt@users.noreply.github.com>
2018-11-19 22:39:35 -0500

    Update problem-set-4.js

--- js/problem-set-4.js
+++ js/problem-set-4.js
@@ -200,11 +200,12 @@ function money() {
   /////////////////////// DO NOT MODIFY
 
   // WRITE YOUR EXERCISE 9 CODE HERE
- let tons=Math.floor(ounces/32000);
-  let pounds=Math.floor((ounces - (tons*32000))/16);
-   ounces=Math.floor((ounces - (tons*32000) - (pounds*16))/1);
-   let div8=document.getElementById("output8")
-   div8.innerHTML=("Tons: " + tons + "</br>Pounds: " + pounds + "</br>Ounces: " + ounces);
+ let dollars=Math.floor(pennies/100);
+  let quarters=Math.floor((pennies-(dollars*100))/25);
+  let dimes=Math.floor((pennies-(dollars*100)-(quarters*25))/10);
+  let nickels=Math.floor((pennies-(dollars*100)-(quarters*25)-(dimes*10))/5);
+  pennies=Math.floor(pennies-(dollars*100)-(quarters*25)-(dimes*10)-(nickels*5));
+  document.getElementById("output9").innerHTML=("Dollars: "+dollars+"</br>Quarters: "+quarters+"</br>Dimes: "+dimes+"</br>Nickels: "+nickels+"</br>Pennies: "+pennies);
   ///////////////////////// DO NOT MODIFY
   check("money", input); // DO NOT MODIFY
   ///////////////////////// DO NOT MODIFY
